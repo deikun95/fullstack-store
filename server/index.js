b41f0dd18083ac218fc4611808ba7430bb455da4
oleg.k <oleg.k@ri.domain>
2021-09-09 17:38:56 +0300

    set controllers / middlewares / error handlers

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
-const models = require('./models/models');
+const models = require('./models');
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
+const router = require('./routes');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
+
+app.use(errorHandler)
 
 const start = async () => {
   try {
