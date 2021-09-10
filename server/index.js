3fe576b8650ff5ad101653b8e8067dae779da7d3
oleg.k <oleg.k@ri.domain>
2021-09-10 15:19:08 +0300

    update deviceController

--- server/index.js
+++ server/index.js
@@ -6,13 +6,17 @@ const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 const router = require('./routes');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const fileUpload = require('express-fileupload');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({ createParentPath: true }));
 app.use('/api', router);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 const start = async () => {
   try {
